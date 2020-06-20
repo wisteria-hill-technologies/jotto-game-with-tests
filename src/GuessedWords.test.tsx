b7eedddea2b8fa330a3f376b1bc2480b34b4b2f4
nfabacus <n.fujioka@abacusmaths.info>
2020-06-20 21:36:21 +0100

    starting to setup GuessWords Component and tests

--- src/GuessedWords.test.tsx
+++ src/GuessedWords.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { findByTestAttr } from './test/testUtils';
+import { findByTestAttr, checkProps } from './test/testUtils';
+import GuessedWords from './GuessedWords';
+
+const defaultProps = {
+  guessedWords: [
+    { guessedWord: 'train', letterMatchCount: 3}
+  ]
+}
+
+const setup = (props={}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords { ...setupProps }/>);
+};
 
 describe('GuessedWords', () => {
-  test('test', () => {
-    expect(true).toBeTruthy();
+  test('does not throw warning with expected props', () => {
+    checkProps(GuessedWords, defaultProps);
   });
 });
--- src/GuessedWords.tsx
+++ src/GuessedWords.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-const GuessedWords: React.FC = (props) => {
+interface Props {
+
+}
+
+const GuessedWords: React.FC<Props> = () => {
   return (
     <div />
   )
 };
 
 GuessedWords.propTypes = {
-
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired
+    })
+  ).isRequired,
 };
 
 export default GuessedWords;
