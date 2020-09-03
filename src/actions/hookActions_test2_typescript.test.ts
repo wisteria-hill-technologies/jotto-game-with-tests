a5de1517ed91af28130ad62db04ed49214154498
nfabacus <n.fujioka@abacusmaths.info>
2020-09-03 08:45:15 +0100

    - another test version for hookActions

---
+++ src/actions/hookActions_test2_typescript.test.ts
@@ -0,0 +1,32 @@
+import fetch from 'jest-fetch-mock';
+const secretWord = 'hello';
+
+import { getSecretWord } from './hookactions';
+
+beforeEach(() => {
+  fetch.resetMocks();
+});
+
+it('getSecretWord', async () => {
+  fetch.mockResponseOnce(JSON.stringify(secretWord));
+
+  //// if fetching multiple times, can mock like below.
+  // fetch.mockResponses(
+  //   [ JSON.stringify(secretWord), { status: 200 }],
+  //   [ JSON.stringify(secretWord), { status: 200 }],
+  // );
+
+  const mockSetSecretWord = jest.fn();
+  await getSecretWord(mockSetSecretWord);
+
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(mockSetSecretWord).toHaveBeenCalledWith(secretWord);
+});
+
+it('handles exception with null', async () => {
+  fetch.mockReject(() => Promise.reject('API Failure'));
+  const mockSetSecretWord = jest.fn();
+  await getSecretWord(mockSetSecretWord);
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(mockSetSecretWord).toHaveBeenCalledWith('');
+});
